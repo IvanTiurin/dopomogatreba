03b26b1e3fc190934c87f5d185538b1bbda7c2ee
IvanTiurin <ivan.tiurin9@gmail.com>
2022-07-06 20:29:56 +0300

    updated to main

---
+++ frontend/src/components/contentBlocks/06_SingleAds/index.js
@@ -0,0 +1,31 @@
+import './index.css'
+
+const SingleAds = () => {
+
+    return (
+        
+        <div>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+            <section className="singleads">
+                <div className="container-fluid d-flex se_item align-items-center justify-content-center">Vivo21 48MP OIS Slefie</div>
+                <div className="container-fluid d-flex se_dir align-items-center justify-content-center">
+                    <nav aria-label="breadcrumb">
+                        <ol className="breadcrumb">
+                            <li className="breadcrumb-item se_nav"><a href="#">Home</a></li>
+                            <li className="breadcrumb-item se_nav"><a href="#">Ads List</a></li>
+                            <li className="breadcrumb-item se_nav"><a href="#">Mobile</a></li>
+                            <li className="breadcrumb-item se_nav"><a href="#">Vivo</a></li>
+                            <li className="breadcrumb-item se_nav"><a href="#">Other Models</a></li>
+                            <li className="breadcrumb-item se_nav"><a href="#">Other Models in New York</a></li>
+                            <li className="breadcrumb-item se_nav_act active text-white" aria-current="page">Vivo21 48MP OIS Slefie</li>
+                        </ol>
+                    </nav>
+                </div>
+            </section>
+        </div>
+        
+    )
+
+}
+
+export default SingleAds
--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import Buysell from './components/contentBlocks/Buy-sell/Index';
 import './App.css';
 
